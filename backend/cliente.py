58761e3159212282b1c97267805ea71551f40571
Stévillis Sousa <stevillis@hotmail.com>
2026-04-14 01:04:49 -0400

    feat: create backend for client related operations

---
+++ backend/cliente.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+
+
+class Cliente:
+    def __init__(self, endereco: str) -> None:
+        self._endereco = endereco
+        self._contas = []
+
+    @property
+    def contas(self) -> list:
+        return self._contas
+
+    def realizar_transacao(self, conta, transacao) -> None:
+        transacao.registrar(conta)
+
+    def adicionar_conta(self, conta) -> None:
+        self._contas.append(conta)
+
+
+class PessoaFisica(Cliente):
+    def __init__(self, nome: str, data_nascimento: str, cpf: str, endereco: str):
+        super().__init__(endereco)
+        self.nome = nome
+        self.data_nascimento = datetime.strptime(data_nascimento, "%d-%m-%Y")
+        self.cpf = cpf
