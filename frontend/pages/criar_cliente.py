e856a266f95d713c60078ff63982370d87bacd71
Stévillis Sousa <stevillis@hotmail.com>
2026-04-17 03:24:35 -0400

    refactor: allow creating new client instead of redirecting to home

--- frontend/pages/criar_cliente.py
+++ frontend/pages/criar_cliente.py
@@ -24,56 +24,63 @@ if __name__ == "__main__":
 
     st.title("Criar Cliente")
 
-    with st.form("cadastro_cliente"):
-        nome = st.text_input(
-            label="Nome Completo:",
-            max_chars=150,
-            placeholder="Informe o nome completo",
-        )
+    if "criar_cliente_form_loaded" not in st.session_state:
+        st.session_state["criar_cliente_form_loaded"] = False
 
-        cpf = st.text_input(
-            label="CPF:",
-            max_chars=11,
-            placeholder="Informe o CPF do cliente",
-        )
+    if not st.session_state["criar_cliente_form_loaded"]:
+        if st.button(label="Novo Cliente", type="secondary", use_container_width=True):
+            st.session_state["criar_cliente_form_loaded"] = True
+            st.rerun()
+    else:
+        with st.form("cadastro_cliente"):
+            nome = st.text_input(
+                label="Nome Completo:",
+                max_chars=150,
+                placeholder="Informe o nome completo",
+            )
 
-        data_nascimento = st.date_input(
-            label="Data de Nascimento:",
-            min_value=date(1900, 1, 1),
-            max_value="today",
-            format="DD/MM/YYYY",
-        )
+            cpf = st.text_input(
+                label="CPF:",
+                max_chars=11,
+                placeholder="Informe o CPF do cliente",
+            )
 
-        endereco = st.text_area(
-            label="Endereço:",
-            max_chars=200,
-            placeholder="Informe o endereço (logradouro, numero - bairro - cidade/sigla estado)",
-        )
+            data_nascimento = st.date_input(
+                label="Data de Nascimento:",
+                min_value=date(1900, 1, 1),
+                max_value="today",
+                format="DD/MM/YYYY",
+            )
 
-        if st.form_submit_button("Criar Cliente", type="secondary"):
-            if not nome:
-                st.error("O Nome é obrigatório!")
-            if not cpf:
-                st.error("O CPF é obrigatório!")
+            endereco = st.text_area(
+                label="Endereço:",
+                max_chars=200,
+                placeholder="Informe o endereço (logradouro, numero - bairro - cidade/sigla estado)",
+            )
 
-            if cpf and nome:
-                cliente_existente = streamlit_controller.filtrar_cliente(cpf)
-                if cliente_existente:
-                    st.error("Já existe cliente com esse CPF!")
-                else:
-                    streamlit_controller.criar_cliente(
-                        nome,
-                        cpf,
-                        data_nascimento.strftime("%d/%m/%Y"),
-                        endereco,
-                    )
+            if st.form_submit_button("Criar Cliente", type="secondary"):
+                if not nome:
+                    st.error("O Nome é obrigatório!")
+                if not cpf:
+                    st.error("O CPF é obrigatório!")
 
-                    st.balloons()
+                if cpf and nome:
+                    cliente_existente = streamlit_controller.filtrar_cliente(cpf)
+                    if cliente_existente:
+                        st.error("Já existe cliente com esse CPF!")
+                    else:
+                        streamlit_controller.criar_cliente(
+                            nome,
+                            cpf,
+                            data_nascimento.strftime("%d/%m/%Y"),
+                            endereco,
+                        )
 
-                    st.success(
-                        "Cliente criado com sucesso! Redirecionando para a página inicial..."
-                    )
+                        st.success("Cliente criado com sucesso!")
+                        time.sleep(2)
+                        del st.session_state["criar_cliente_form_loaded"]
+                        st.rerun()
 
-                    time.sleep(3)
-
-                    st.switch_page("pages/home.py")
+        if st.button(label="Voltar", use_container_width=True, type="tertiary"):
+            del st.session_state["criar_cliente_form_loaded"]
+            st.rerun()
